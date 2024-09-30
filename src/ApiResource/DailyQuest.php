6fe801487f2dab80f8f41038022cbf6a4cf4f4af
Schizy <ali.mechichi@gmail.com>
2024-09-30 15:39:31 +0200

    Chapitre 8

--- src/ApiResource/DailyQuest.php
+++ src/ApiResource/DailyQuest.php
@@ -3,8 +3,21 @@
 namespace App\ApiResource;
 
 use ApiPlatform\Metadata\ApiResource;
+use App\State\DailyQuestStateProvider;
 
-#[ApiResource]
+#[ApiResource(
+    shortName: 'Quest',
+    provider: DailyQuestStateProvider::class,
+)]
 class DailyQuest
 {
+    public int $id;
+
+    /**
+     * @param int $id
+     */
+    public function __construct(int $id)
+    {
+        $this->id = $id;
+    }
 }
