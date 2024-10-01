b56bc9c0baf91d2b67401210650680b21722c484
Schizy <ali.mechichi@gmail.com>
2024-10-01 16:03:12 +0200

    Chapitre 19

--- src/State/EntityToDtoStateProvider.php
+++ src/State/EntityToDtoStateProvider.php
@@ -4,6 +4,8 @@ namespace App\State;
 
 use ApiPlatform\Doctrine\Orm\Paginator;
 use ApiPlatform\Doctrine\Orm\State\CollectionProvider;
+use ApiPlatform\Doctrine\Orm\State\ItemProvider;
+use ApiPlatform\Metadata\CollectionOperationInterface;
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\Pagination\TraversablePaginator;
 use ApiPlatform\State\ProviderInterface;
@@ -14,27 +16,30 @@ use Symfony\Component\DependencyInjection\Attribute\Autowire;
 class EntityToDtoStateProvider implements ProviderInterface
 {
     public function __construct(
-        #[Autowire(service: CollectionProvider::class)]
-        private readonly ProviderInterface $collectionProvider,
+        #[Autowire(service: CollectionProvider::class)] private readonly ProviderInterface $collectionProvider,
+        #[Autowire(service: ItemProvider::class)] private readonly ProviderInterface       $itemProvider,
     )
     {
     }
 
     public function provide(Operation $operation, array $uriVariables = [], array $context = []): object|array|null
     {
-        $entities = $this->collectionProvider->provide($operation, $uriVariables, $context);
+        if ($operation instanceof CollectionOperationInterface) {
+            $entities = $this->collectionProvider->provide($operation, $uriVariables, $context);
 
-        $dtos = [];
-        foreach ($entities as $entity) {
-            $dtos[] = $this->mapEntityToDto($entity);
+            $dtos = [];
+            foreach ($entities as $entity) {
+                $dtos[] = $this->mapEntityToDto($entity);
+            }
+
+            return new TraversablePaginator(
+                new ArrayIterator($dtos), $entities->getCurrentPage(), $entities->getItemsPerPage(), $entities->getTotalItems(),
+            );
         }
 
-        return new TraversablePaginator(
-            new ArrayIterator($dtos),
-            $entities->getCurrentPage(),
-            $entities->getItemsPerPage(),
-            $entities->getTotalItems(),
-        );
+        $entity = $this->itemProvider->provide($operation, $uriVariables, $context);
+
+        return $entity ? $this->mapEntityToDto($entity) : null;
     }
 
     private function mapEntityToDto(object $entity): object
@@ -43,7 +48,7 @@ class EntityToDtoStateProvider implements ProviderInterface
         $dto->id = $entity->getId();
         $dto->email = $entity->getEmail();
         $dto->username = $entity->getUsername();
-        $dto->dragonTreasures = $entity->getDragonTreasures()->toArray();
+        $dto->dragonTreasures = $entity->getPublishedDragonTreasures()->toArray();
 
         $dto->flameThrowingDistance = rand(1, 10);
 
