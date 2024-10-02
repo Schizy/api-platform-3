9f7891374e94e7690b7a69a13d0dd530ce8e8a92
Schizy <ali.mechichi@gmail.com>
2024-10-02 15:28:29 +0200

    Chapitre 23 & 24

--- src/ApiResource/UserApi.php
+++ src/ApiResource/UserApi.php
@@ -7,14 +7,32 @@ use ApiPlatform\Doctrine\Orm\State\Options;
 use ApiPlatform\Metadata\ApiFilter;
 use ApiPlatform\Metadata\ApiProperty;
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
 use App\Entity\DragonTreasure;
 use App\Entity\User;
 use App\State\EntityClassDtoStateProcessor;
 use App\State\EntityToDtoStateProvider;
+use Symfony\Component\Serializer\Annotation\Ignore;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ApiResource(
     shortName: 'User',
+    operations: [
+        new Get(),
+        new GetCollection(),
+        new Post(
+            security: 'is_granted("PUBLIC_ACCESS")',
+            validationContext: ['groups' => ['Default', 'postValidation']], //We add a special group only for POST
+        ),
+        new Patch(security: 'is_granted("ROLE_USER_EDIT")'),
+        new Delete(),
+    ],
     paginationItemsPerPage: 5,
+    security: 'is_granted("ROLE_USER")',
     provider: EntityToDtoStateProvider::class,
     processor: EntityClassDtoStateProcessor::class,
     stateOptions: new Options(entityClass: User::class),
@@ -24,24 +42,33 @@ use App\State\EntityToDtoStateProvider;
 ])]
 class UserApi
 {
-    #[ApiProperty(readable: false, identifier: true)]
+    /**
+     * The #[Ignore] attribute will ignore the field entirely (not writeable nor readable)
+     */
+    #[Ignore]
     public ?int $id = null;
 
+    #[Assert\NotBlank]
+    #[Assert\Email]
     public ?string $email = null;
 
+    #[Assert\NotBlank]
     public ?string $username = null;
 
     /**
      * The plaintext password when being set or changed
-     * The api property makes it only writeable
+     * The ApiProperty attribute makes it only writeable
      */
     #[ApiProperty(readable: false)]
+    #[Assert\NotBlank(groups: ['postValidation'])] //We can have it blank in every operation except POST
     public ?string $password = null;
 
     /**
      * @var array<int, DragonTreasure>
      */
+    #[ApiProperty(writable: false)]
     public array $dragonTreasures = [];
 
+    #[ApiProperty(writable: false)]
     public int $flameThrowingDistance = 0;
 }
