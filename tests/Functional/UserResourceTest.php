6bbbefdbf464c8ceea3acdf43ea3e258fd8b2f1a
Schizy <ali.mechichi@gmail.com>
2024-10-06 16:55:26 +0200

    Chapitre 34

--- tests/Functional/UserResourceTest.php
+++ tests/Functional/UserResourceTest.php
@@ -20,17 +20,16 @@ class UserResourceTest extends ApiTestCase
                     'email' => 'draggin_in_the_morning@coffee.com',
                     'username' => 'draggin_in_the_morning',
                     'password' => 'password',
-                ]
+                ],
             ])
             ->assertStatus(201)
             ->post('/login', [
                 'json' => [
                     'email' => 'draggin_in_the_morning@coffee.com',
                     'password' => 'password',
-                ]
+                ],
             ])
-            ->assertSuccessful()
-        ;
+            ->assertSuccessful();
     }
 
     public function testPatchToUpdateUser(): void
@@ -43,7 +42,7 @@ class UserResourceTest extends ApiTestCase
                 'json' => [
                     'username' => 'changed',
                 ],
-                'headers' => ['Content-Type' => 'application/merge-patch+json']
+                'headers' => ['Content-Type' => 'application/merge-patch+json'],
             ])
             ->assertStatus(200);
     }
@@ -59,13 +58,42 @@ class UserResourceTest extends ApiTestCase
             ->patch('/api/users/' . $user->getId(), [
                 'json' => [
                     'username' => 'changed',
-                    'dragonTreasures' => [
+                    'dragonTreasures' => [ //It's easier to make dragonTreasures not writeable here and PATCH the treasures directly
                         '/api/treasures/' . $dragonTreasure->getId(),
                     ],
                 ],
-                'headers' => ['Content-Type' => 'application/merge-patch+json']
+                'headers' => ['Content-Type' => 'application/merge-patch+json'],
             ])
             ->assertStatus(422);
+    }
+
+    public function testTreasuresCanBeRemoved(): void
+    {
+        $user = UserFactory::createOne();
+        $dragonTreasure1 = DragonTreasureFactory::createOne(['owner' => $user]);
+        DragonTreasureFactory::createOne(['owner' => $user]);
+
+        $otherUser = UserFactory::createOne();
+        $dragonTreasure3 = DragonTreasureFactory::createOne(['owner' => $otherUser]);
+
+        $this->browser()
+            ->actingAs($user)
+            ->patch('/api/users/' . $user->getId(), [
+                'json' => [
+                    'dragonTreasures' => [
+                        '/api/treasures/' . $dragonTreasure1->getId(),
+                        '/api/treasures/' . $dragonTreasure3->getId(),
+                    ],
+                ],
+                'headers' => ['Content-Type' => 'application/merge-patch+json'],
+            ])
+            ->assertStatus(200)
+            ->get('/api/users/' . $user->getId())
+            ->assertJsonMatches('length("dragonTreasures")', 2)
+            ->assertJsonMatches('dragonTreasures[0]', '/api/dragonTreasures/' . $dragonTreasure1->getId())
+
+            //Fails because we can't write yet
+            ->assertJsonMatches('dragonTreasures[1]', '/api/dragonTreasures/' . $dragonTreasure3->getId());
     }
 
     public function testUnpublishedTreasuresNotReturned(): void
