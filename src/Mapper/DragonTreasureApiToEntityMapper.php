5523964d47439b68599bef7a5e19504c8352d913
Schizy <ali.mechichi@gmail.com>
2024-10-06 16:37:24 +0200

    Chapitre 33

--- src/Mapper/DragonTreasureApiToEntityMapper.php
+++ src/Mapper/DragonTreasureApiToEntityMapper.php
@@ -4,17 +4,22 @@ namespace App\Mapper;
 
 use App\ApiResource\DragonTreasureApi;
 use App\Entity\DragonTreasure;
+use App\Entity\User;
 use App\Repository\DragonTreasureRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Exception;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfonycasts\MicroMapper\AsMapper;
 use Symfonycasts\MicroMapper\MapperInterface;
+use Symfonycasts\MicroMapper\MicroMapperInterface;
 
 #[AsMapper(from: DragonTreasureApi::class, to: DragonTreasure::class)]
 class DragonTreasureApiToEntityMapper implements MapperInterface
 {
-    public function __construct(private readonly DragonTreasureRepository $repository, private readonly Security $security)
+    public function __construct(
+        private readonly DragonTreasureRepository $repository,
+        private readonly Security                 $security,
+        private readonly MicroMapperInterface     $microMapper,
+    )
     {
     }
 
@@ -35,7 +40,10 @@ class DragonTreasureApiToEntityMapper implements MapperInterface
         $entity = $to;
 
         if ($dto->owner) {
-            //TODO
+            // We always need to set the max depth to 0 when we map a RELATION object, we just want to query it
+            $entity->setOwner($this->microMapper->map($dto->owner, User::class, [
+                MicroMapperInterface::MAX_DEPTH => 0,
+            ]));
         } else {
             $entity->setOwner($this->security->getUser());
         }
